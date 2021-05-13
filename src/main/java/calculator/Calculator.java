48d089ab13c85b726ce6733394730da5e2351a6a
Rouzbeh Meshkinnejad <rouzyd@gmail.com>
2021-05-13 23:27:05 +0430

    Simple scenarios written and tests were successfully passed

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,27 @@ public class Calculator {
     public int add(int value1, int value2) {
         return value1 + value2;
     }
+
+    public int apply(int value1, int value2, char op) {
+        if (op == '/')
+        {
+            return div(value1, value2);
+        }
+        else
+            return power(value1, value2);
+    }
+
+    private int div(int value1, int value2) {
+        return value1/value2;
+    }
+
+    private int power(int value1, int value2)
+    {
+        if (value2 == 0)
+            return 1;
+        int ret = 1;
+        for (int i = 0; i < value2; i++)
+            ret = ret * value1;
+        return ret;
+    }
 }
--- src/test/java/calculator/MyStepdefs.java
+++ src/test/java/calculator/MyStepdefs.java
@@ -11,6 +11,7 @@ public class MyStepdefs {
     private int value1;
     private int value2;
     private int result;
+    private char calc_op;
 
     @Before
     public void before() {
@@ -26,11 +27,22 @@ public class MyStepdefs {
     @When("^I add the two values$")
     public void iAddTheTwoValues() {
         result = calculator.add(value1, value2);
-        System.out.println(result);
     }
 
     @Then("^I expect the result (-?\\d+)$")
     public void iExpectTheResult(int arg0) {
         Assert.assertEquals(arg0, result);
+    }
+
+    @Given("^Two input values, (-?\\d+) and (-?\\d+), and an operation ([/^])$")
+    public void twoInputValuesAndAndAnOperation(int arg0, int arg1, char op) {
+        value1 = arg0;
+        value2 = arg1;
+        calc_op = op;
+    }
+
+    @When("^I apply the operation$")
+    public void iApplyTheOperation() {
+        result = calculator.apply(value1, value2, calc_op);
     }
 }
